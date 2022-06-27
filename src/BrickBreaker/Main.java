e01556c81744929a4391ce9acf00e15a2e4f1c36
Akash Dhingra <akashdhingra1996@gmail.com>
2022-06-26 21:49:31 -0400

    created gameplay class, added keylistners and actionlistner along with their methods

--- src/BrickBreaker/Main.java
+++ src/BrickBreaker/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
         obj.setBounds(10, 10, 700, 600);        // size of frame
         obj.setTitle("Breakout Game");                             // Title of frame
         obj.setResizable(false);                                   // Resizable frame
         obj.setVisible(true);                                      // visibility of frame
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // close on pressing exit button
+        obj.add(gameplay);
     }
 }
